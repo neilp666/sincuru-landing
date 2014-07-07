1e53d75484544e47340c883e973923bd7b9c32d6
neil patel <neil@myindianweddingapp.com>
2014-07-07 15:26:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+== README
+
+Sincuru Landing Page
+
+Version 1.0
+
+* Ruby version 2.0
+
+* Rails 4.0.4
+
+* Bootstrap 3.0
+
+* MailChimp API
+
+* Dropbox API 
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sincurulanding::Application.config.secret_key_base = 'ee1e696ee7f4cddea8925d5b6a000523e6de3998001605bd8d33b066f053bcf3ed1052b8ff0b5c998a6096e1c2305fcb5c625f73476e0d868e11811e3d81937c'
